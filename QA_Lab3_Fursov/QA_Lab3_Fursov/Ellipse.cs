d01968d12bd437a1f757aa3fcc71b583df23e79d
Alexandr Fursov <143800867+xFufik@users.noreply.github.com>
2026-04-29 13:39:04 +0700

    Add files via upload

---
+++ QA_Lab3_Fursov/QA_Lab3_Fursov/Ellipse.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace QA_Lab3_Fursov
+{
+    public class Ellipse
+    {
+        protected double a; // большая полуось
+        protected double b; // малая полуось 
+
+        public Ellipse()
+        {
+            a = 2;
+            b = 1;
+        }
+
+        public Ellipse(double A, double B)
+        {
+            Init(A, B);
+        }
+
+        public void Init(double A, double B)
+        {
+            if (A > B && B > 0)
+            {
+                a = A;
+                b = B;
+            }
+            else
+            {
+                Console.WriteLine("Некорректные значения (a > b > 0). Установлены значения по умолчанию.");
+                a = 2;
+                b = 1;
+            }
+        }
+
+        // геттеры для доступа к полям
+        public double GetA() { return a; }
+        public double GetB() { return b; }
+
+        public void Read()
+        {
+            Console.Write("Введите большую полуось a: ");
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите малую полуось b: ");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            Init(A, B);
+        }
+
+        public virtual void Display()
+        {
+            Console.Write($"Эллипс: a = {a}, b = {b}");
+        }
+
+        public virtual double Area()
+        {
+            return Math.PI * a * b;
+        }
+
+        public static Ellipse add(Ellipse e1, Ellipse e2)
+        {
+            Ellipse res = new Ellipse();
+            res.Init(e1.a + e2.a, e1.b + e2.b);
+            return res;
+        }
+    }
+
+    // Производный класс
+    public class Ellipse3D : Ellipse
+    {
+        private double z;
+
+        public Ellipse3D() : base()  // вызов конструктора базового класса
+        {
+            z = 1;
+        }
+
+        public Ellipse3D(double A, double B, double Z) : base(A, B)  // вызов базового конструктора
+        {
+            Put(Z);
+        }
+
+        // методы Get и Put для работы с полем z
+        public double GetZ()
+        {
+            return z;
+        }
+
+        public void Put(double Z)
+        {
+            z = Z;
+        }
+
+        // перегрузка Init 
+        public void Init(double A, double B, double Z)
+        {
+            base.Init(A, B);
+            Put(Z);
+        }
+
+        // перегрузка Display
+        public override void Display()
+        {
+            base.Display();
+            Console.Write($", z = {z}");
+        }
+
+        // перегрузка метода Area
+        public override double Area()
+        {
+            return Math.PI * a * b * z;
+        }
+
+        public static Ellipse3D add3D(Ellipse3D e1, Ellipse3D e2)
+        {
+            Ellipse3D result = new Ellipse3D();
+            result.Init(e1.a + e2.a, e1.b + e2.b, e1.z + e2.z);
+            return result;
+        }
+    }
+}
---
+++ QA_Lab3_Fursov/QA_Lab3_Fursov/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace QA_Lab3_Fursov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Базовый класс Ellipse ");
+
+            // g. статические объекты базового класса
+            Ellipse e1 = new Ellipse(3, 2);
+            Ellipse e2 = new Ellipse();
+
+            Console.WriteLine("\nСтатический объект e1 (конструктор с параметрами):");
+            e1.Display();
+            Console.WriteLine($"\nПлощадь: {e1.Area()}");
+
+            Console.WriteLine("\nВвод параметров для e2:");
+            e2.Read();
+            e2.Display();
+            Console.WriteLine($"\nПлощадь: {e2.Area()}");
+
+            // g. статические объекты производного класса
+            Ellipse3D e3d1 = new Ellipse3D(4, 3, 2);
+            Ellipse3D e3d2 = new Ellipse3D();
+
+            Console.WriteLine("\nСтатический объект e3d1 (конструктор с параметрами):");
+            e3d1.Display();
+            Console.WriteLine($"\nПлощадь 3D: {e3d1.Area()}");
+
+            Console.WriteLine("\nВвод параметров для e3d2:");
+            Console.Write("Введите большую полуось a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите малую полуось b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите расстояние z: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            e3d2.Init(a, b, z);
+            e3d2.Display();
+            Console.WriteLine($"\nПлощадь 3D: {e3d2.Area()}");
+        }
+    }
+}
